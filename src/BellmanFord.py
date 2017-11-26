c9b6ded157d3ccb0ed8090fd6de5c5c1261782ac
Branden Kim <brandenk0@gmail.com>
2017-11-26 17:18:34 -0500

    Create test cases & update BF algorithm

--- src/BellmanFord.py
+++ src/BellmanFord.py
@@ -6,17 +6,58 @@ dict to represent Graph:
     a --> b (weight of 5)
     shown as 'a': [('b', 5)]
 '''
+import collections
 
 
-
-def bellman_ford(graph, src):
+def bellman_ford(graph, src, target=None):
     '''
+    * Calculates shortest distances and previous nodes for each node
+
     :param graph: dict containing node: (edge, weight) pairs
                   represents the directed graph
-    :param src: char representing the source node to start with
+
+    :param src: int representing the source node to start with
+
+    :param target: Optional param giving target node to find shortest
+                   distance to from src
+
     :return: None if negative cycle detected, else array with shortest 
-             paths is returned  
+             distances and paths is returned or shortest dist to target
+             if target param is given  
     '''
+    # List containing distances and previous node for shortest route
+    d = [float('Inf') for v in graph]
+    d[src] = 0
+
+    # Loop through the graph finding the 
+    # shortest paths with 'k' hops
+    for k in range(1, len(graph) - 1):
+        # Check for each vertex within the graph
+        for v in graph:
+            # Grab each edge connected to 'v' & their weight
+            for u, w in graph[v]:
+                # Update weight & previous node
+                # if there's a shorter path
+                dist_v = d[v]
+                dist_u = d[u]
+                if dist_v + w < dist_u:
+                    d[u] = dist_v + w
+
+    # Check for any negative cycles
+    for v in graph:
+        for u, w in graph[v]:
+            # Check if 'n-th' hop creates a shorter dist
+            dist_v = d[v]
+            dist_u = d[u]
+            if dist_v + w < dist_u:
+                # print('Negative Cycle')
+                return
+    if target is not None:
+        return d[target]
+    
+    return {node: d[node] for node in graph}
+
+def construct_paths(graph, src):
     # List containing distances and previous node for shortest route
     d = [(float('Inf'), None) for v in graph]
     d[src] = (0, src)
@@ -42,20 +83,68 @@ def bellman_ford(graph, src):
             dist_v = d[v][0]
             dist_u = d[u][0]
             if dist_v + w < dist_u:
-                print('Negative Cycle')
+                # print('Negative Cycle')
                 return
-
+    
     return d
+
+def bf_paths(graph, src, target=None):
+    '''
+    * Constructs shortest paths for every node in graph
+
+    :param graph: dict containing (node: (edge, weight)) pairs
+                  representing the directed graph
+    
+    :param src: int representing the source node to start with
+    
+    :return dict: Contains the shortest paths for each node in graph
+                  if target is given, then returns shortest path from
+                  src to target node
+    '''
+    # Get the shortest distances and previous node for each node
+    d = construct_paths(graph, src)
+
+    if d is None:
+        return
+
+    # Construct shortest path route
+    shortest_paths = {node: [] for node in graph}
+
+    for node in shortest_paths:
+        path = collections.deque()
+        path.append(node)
+        prev = d[node][1]
+
+        if prev == 0 and node != 0:
+            path.appendleft(prev)
+        elif node != 0:
+            while prev != src:
+                path.appendleft(prev)
+                prev = d[prev][1]
+            path.appendleft(src)
+
+        shortest_paths[node] = list(path)
+
+    if target is not None:
+        return shortest_paths[target]
+
+    return shortest_paths
+
 
 
 if __name__ == '__main__':
     g = {0: [(1, 2)],
-         1: [(2, 8), (3, 5)],
-         2: [(3, -5)],
-         3: []
-    }
+             1: [(2, 8), (3, 5)],
+             2: [(3, -5)],
+             3: [(4, 4), (5, -2), (6, 1)],
+             4: [],
+             5: [(6, 6)],
+             6: []
+        }
 
     print(bellman_ford(g, 0))
+    print(bf_paths(g, 0))
+    print(bellman_ford(g, 0, target=5))
 
 
 
---
+++ tests/test_alg.py
@@ -0,0 +1,126 @@
+import unittest
+import random
+import networkx as nx
+import src.BellmanFord as BellmanFord
+
+def construct_nx_graph(g):
+    '''
+    * Constructs the directed graph using the NetworkX library
+    * Adds all nodes, edges, and weights from input dict
+    :param g: input dictionary representing graph
+    :return dg: Directed graph using networkx library
+    '''
+    dg = nx.DiGraph()
+
+    for node in g:
+        dg.add_node(node)
+
+    for node in g:
+        for edge, weight in g[node]:
+            dg.add_edge(node, edge, weight=weight)
+
+    return dg
+
+def random_graphs():
+    g = {0: [(1, rand_weight())],
+         1: [(2, rand_weight()), (3, rand_weight())],
+         2: [(3, rand_weight())],
+         3: [(4, rand_weight()), (5, rand_weight()), (6, rand_weight())],
+         4: [],
+         5: [(6, rand_weight())],
+         6: []
+    }
+
+    return (g, construct_nx_graph(g))
+
+def rand_weight(start=1, end=20):
+    return random.randint(start, end)
+
+class TestAlg(unittest.TestCase):
+    def test_dist_to_target(self):
+        '''
+        * Generates set directed graph with random weights
+        * Checks correctness of algorithm to library's for 
+          shortest distance to target node
+        '''
+        g, nx_g = random_graphs()
+
+        nx_dist = nx.bellman_ford_path_length(nx_g, 0, 6)
+        my_dist = BellmanFord.bellman_ford(g, 0, target=6)
+
+        self.assertEqual(nx_dist, my_dist)
+
+    def test_dist_to_all(self):
+        '''
+        * Generates set directed graph with random weights
+        * Checks correctness of algorithm to library's for 
+          shortest distance to all nodes
+        '''
+        g, nx_g = random_graphs()
+
+        nx_dists = nx.single_source_bellman_ford_path_length(nx_g, 0)
+        my_dists = BellmanFord.bellman_ford(g, 0)
+
+        check = True
+
+        for node in g:
+            if nx_dists[node] != my_dists[node]:
+                check = False
+
+        self.assertTrue(check)
+    
+    def test_path_to_target(self):
+        '''
+        * Generates set directed graph with random weights
+        * Checks correctness of algorithm to library's for 
+          shortest path to target node
+        '''
+        g, nx_g = random_graphs()
+
+        my_path = BellmanFord.bf_paths(g, 0, target=6)
+        nx_path = nx.bellman_ford_path(nx_g, 0, 6)
+
+        self.assertEqual(my_path, nx_path)
+
+    def test_path_to_all(self):
+        '''
+        * Generates set directed graph with random weights
+        * Checks correctness of algorithm to library's for 
+          shortest path to all nodes
+        '''
+        g, nx_g = random_graphs()
+
+        nx_paths = nx.single_source_bellman_ford_path(nx_g, 0)
+        my_paths = BellmanFord.bf_paths(g, 0)
+
+        check = True
+        for node in g:
+            if nx_paths[node] != my_paths[node]:
+                check = False
+
+        self.assertTrue(check)
+
+
+    def test_neg_cycle(self):
+        g = {0: [(1, 2)],
+             1: [(2, 8), (3, 5)],
+             2: [(3, -5)],
+             3: [(4, 4), (5, -2), (6, 1)],
+             4: [(3, -5)],
+             5: [(6, 6)],
+             6: []
+        }
+        self.assertIsNone(BellmanFord.bellman_ford(g, 0))
+
+    def test_neg_cycle_2(self):
+        g = {0: [(1, 2)],
+             1: [(2, 8), (3, 5)],
+             2: [(3, -5)],
+             3: [(2, 2)]
+        }
+        self.assertIsNone(BellmanFord.bellman_ford(g, 0))
+
+
+
+if __name__ == '__main__':
+    unittest.main()
